b4e060866cb3372e34e87485a13faaf751f2b033
Daniel Gavrilov <dgavrilov95@gmail.com>
2019-02-20 22:37:04 -0800

    Upgrade Stripe API version

--- src/lambda/donate.ts
+++ src/lambda/donate.ts
@@ -22,7 +22,7 @@ interface DonationRequest {
     cancelUrl: string
 }
 
-stripe.setApiVersion('2019-02-11; checkout_sessions_beta=v4')
+stripe.setApiVersion('2019-02-19')
 
 async function createSession(donation: DonationRequest) {
     if (donation.amount == null) throw { status: 400, message: "Please specify an amount" }
